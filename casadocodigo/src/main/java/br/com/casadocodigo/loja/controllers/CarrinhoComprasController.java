c44d1ad29d6613030f2464495df246c501ec4eff
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2018-12-18 09:01:12 -0200

    Aula 12 - Escopo sessão

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -1,8 +1,10 @@
 package br.com.casadocodigo.loja.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
@@ -13,11 +15,12 @@ import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 @RequestMapping("/carrinho")
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class CarrinhoComprasController {
 
 	@Autowired
 	private ProdutoDAO produtoDAO;
-	
+
 	@Autowired
 	private CarrinhoCompras carrinho;
 
@@ -25,7 +28,7 @@ public class CarrinhoComprasController {
 	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
 		ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
 		CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
-		
+
 		carrinho.add(carrinhoItem);
 
 		return modelAndView;
@@ -36,7 +39,5 @@ public class CarrinhoComprasController {
 		CarrinhoItem carrinhoItem = new CarrinhoItem(produto, tipoPreco);
 		return carrinhoItem;
 	}
-	
-	
 
 }
